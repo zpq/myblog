b8b0c61265d1529a8a39ad9453c0e47c06d51d69
zpq <307154456@qq.com>
2016-01-08 18:14:16 +0800

    add duoshuo comment plugin

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -10,7 +10,7 @@
 </article>
 
 
-<div id="disqus_thread"></div>
+<!-- <div id="disqus_thread"></div>
 <script>
     /**
      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
@@ -33,5 +33,25 @@
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 
-</script>
+</script> -->
+
+
+<!-- 多说评论框 start -->
+<div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title }}" data-url="{{ $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] }}"></div>
+<!-- 多说评论框 end -->
+<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+<script type="text/javascript">
+var duoshuoQuery = {short_name:"sheaned"};
+    (function() {
+        var ds = document.createElement('script');
+        ds.type = 'text/javascript';ds.async = true;
+        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+        ds.charset = 'UTF-8';
+        (document.getElementsByTagName('head')[0] 
+         || document.getElementsByTagName('body')[0]).appendChild(ds);
+    })();
+    </script>
+<!-- 多说公共JS代码 end -->
+
+
 @stop
