0fbe7ed74b30fd0ea958aef6d492ca1c1128f4e4
zpq <307154456@qq.com>
2016-01-16 22:28:57 +0800

    add baidu verify

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
- 	
+ 	<meta name="baidu-site-verification" content="uPrdE5kLPY" />
     <!-- 引入 Bootstrap -->
 	<link href="http://apps.bdimg.com/libs/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet">
 	<script src="http://apps.bdimg.com/libs/jquery/2.0.0/jquery.min.js"></script>
