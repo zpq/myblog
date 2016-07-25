e4f265ec688700205b21f7f78e93285cc572e7a5
zhaopanqi <zpq@zhaopanqis-iMac.local>
2016-07-25 23:03:52 +0800

    add redis cache

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 // use App\Http\Requests;
 // use App\Http\Requests\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis as Redis;
 use App\Articles;
 use Carbon;
 use YuanChao\Editor\EndaEditor;
@@ -26,27 +27,35 @@ class ArticleController extends Controller
 // 		可以自定义翻页uri.  举例 http://example.com/custom/url?page=N
 		return view('articles.index')->with('articles', $articles);
 	}
-	
+
 	public function show($id)
-	{    	
-		$article = Articles::find($id);
-		if(!$article) {
-			return view('errors.404');
-		}
-		$article->content = EndaEditor::MarkDecode($article->content);
+	{
+        $redis = Redis::connection('default');
+        $article = $redis->get("article_$id");
+        if ($article) {
+
+        } else {
+            $article = Articles::find($id);
+    		if(!$article) {
+    			return view('errors.404');
+    		}
+    		$article->content = EndaEditor::MarkDecode($article->content);
+            $redis->set("article_$id", $article_);
+        }
+
 		return view('articles.show')->with('article', $article);
 	}
-	
+
 // 	public function create() {
 // 		return view('articles.create');
 // 	}
-	
+
 // 	public function store(ArticlesForm $request) {
 // 		$input = $request->all();
 // 		$input['published_at'] = Carbon\Carbon::now();
 // 		Articles::create($input);
 // 		return redirect('articles');
 // 	}
-	
-	
+
+
 }
