4c9a2bbdec3e7dfa909056e796f7db5caf544b85
zhaopanqi <zhaopanqi@china-qizhi.com>
2016-08-31 17:23:36 +0800

    add highlight.js

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,6 +1,9 @@
 @extends('app')
 
 @section('content')
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/styles/default.min.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.6.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
 <h1 class='text-center'>{{ $article->title }}</h1>
 <hr />
 <article>
@@ -11,7 +14,7 @@
 
 
 <!-- 多说评论框 start -->
-<div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title }}" data-url="{{ $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] }}"></div>
+{{-- <div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title }}" data-url="{{ $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] }}"></div> --}}
 <!-- 多说评论框 end -->
 
 @stop
